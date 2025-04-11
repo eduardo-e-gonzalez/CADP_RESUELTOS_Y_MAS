16d7897581ce77273cf7b0847fab960ab4a0c29f
Eduardo Gonzalez <99824488+eduardo-e-gonzalez@users.noreply.github.com>
2025-04-11 15:45:57 -0300

    Update ejercicio1.pas

--- Practica2/Segunda Parte/ejercicio1.pas
+++ Practica2/Segunda Parte/ejercicio1.pas
@@ -21,12 +21,16 @@ End.
 a. ¿Qué imprime si se lee el valor 10 en la variable x ?
 R= va imprimir 11+13+16+20+25= [85;]
 
-b. ¿Qué imprime si se lee el valor 10 en la variable x y se cambia el encabezado del procedure por:
+b. ¿Qué imprime si se lee el valor 10 en la variable x y 
+se cambia el encabezado del procedure por:
 procedure suma(num1: integer; num2:integer);
-R= va imprimir el mismo valor que introdujo el usuario, ya que los 2 parametros estan pasados por valor;
+R= va imprimir el mismo valor que introdujo el usuario,
+ya que los 2 parametros estan pasados por valor;
 
 c. ¿Qué sucede si se cambia el encabezado del procedure por:
 procedure suma(var num1: integer; var num2:integer);
-R= va a ser 1 + x + 1 + x + 1 + x + 1 + x + infinitamente, ya que los 2 parametros estan pasados por referencia; y el indice se inicializa en 0 cada vez que se llama al procedure;
+R= va a ser 1 + x + 1 + x + 1 + x + 1 + x + infinitamente, 
+ya que los 2 parametros estan pasados por referencia; y el indice se inicializa en 0 
+cada vez que se llama al procedure;
 De igual manera no deberia compilar, ya que el indice se inicializa en 0 cada vez que se llama al procedure;
 }
